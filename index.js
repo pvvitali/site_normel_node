8809c884ac06903f1b96f14e677d8dc497eafca6
Vitali <vitali276@yandex.ru>
2022-10-21 14:14:25 +0200

    add text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 8080;
 
 const server = http.createServer( (req, res) => {
-    res.end("Hello Vitali");
+    res.end("Hello Vitali nodejs server");
 });
 
 server.listen(port, () => {
